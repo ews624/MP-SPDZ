34643f060d44ad1f6beff229f05e7ba1a9b30225
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2023-06-09 14:45:10 -0500

    Uploading final updates from Wei

--- Programs/Source/python_tree_builder.py
+++ Programs/Source/python_tree_builder.py
@@ -18,6 +18,8 @@ class Node(object):
 def preorder(Node):
     if Node:
         print(Node.name)
+        print(Node.value)
+        print(Node.shape)
         if Node.children:
             preorder(Node.children[0])
             preorder(Node.children[1])
@@ -32,9 +34,6 @@ os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-pa
 
 string = stream.read()
 
-stringtoint = int(string)
-
-test = "1"
 print("The largest index is:", string)
 
 command = "./compile.py -l unique_values "+ string
@@ -42,7 +41,6 @@ command = "./compile.py -l unique_values "+ string
 print("Command is "+command)
 
 os.system(command)
-#time.sleep(4)
 stream = os.popen('./shamir-party.x -p 0 unique_values-'+string)
 os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 1 unique_values-"+string+"; sleep 4\" '")
 os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 2 unique_values-"+string+"; sleep 4\" '")
@@ -50,7 +48,7 @@ unique_values = stream.read()
 
 print("The amount of unique_values is "+unique_values)
 
-root = Node("root","",shapeTuple[0])
+root = Node("root","root",shapeTuple[0])
 string = string.rstrip('\n')
 for i in range(0,int(unique_values)):
     root.add_child(Node(str(1)+"-"+string+"-"+str(i+1)+"-"+shapeTuple[0],i+1,shapeTuple[0]))
@@ -66,7 +64,7 @@ preorder(root)
 for i in range(1,int(unique_values)+1):
 
     string = str(i)
-    command = "./compile.py -l unique_values "+ string
+    command = "./compile.py -l Node_entropy "+ string
 
     print("Command is "+command)
 
@@ -80,8 +78,44 @@ for i in range(1,int(unique_values)+1):
 
     if int(sameShapes) == 0:
         print("ITS ZERO \n\n\n\n\n")
+        string = str(i)
+        command = "./compile.py -l find_shape "+ string
+
+        print("Command is "+command)
+        stream = os.popen('./shamir-party.x -p 0 find_shape-'+string)
+        os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 1 find_shape-"+string+"; sleep 2\" '")
+        os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 2 find_shape-"+string+"; sleep 2\" '")
+        whatShape =stream.read()
+        whatShape = whatShape.rstrip('\n')
+        print("What shape int is :"+shapeTuple[int(whatShape)])
+        print("\n\n\n\n\n")
+        root.children[i-1].shape = shapeTuple[int(whatShape)]
         #get Shape once entropy is zero
-    #else: continue the tree
+    else:
+        print_ln
+        ColorInt = str(i)
+        command = "./compile.py -l deeper_nodes "+ ColorInt
+
+        print("Command is "+command)
+        stream = os.popen('./shamir-party.x -p 0 deeper_nodes-'+ColorInt)
+        os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 1 deeper_nodes-"+ColorInt+"; sleep 2\" '")
+        os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 2 deeper_nodes-"+ColorInt+"; sleep 2\" '")
+        column_number =stream.read()
+        column_number = column_number.rstrip('\n')
+        command = "./compile.py -l unique_values "+ column_number
+
+        print("Command is "+command)
+
+        os.system(command)
+        stream = os.popen('./shamir-party.x -p 0 unique_values-'+column_number)
+        os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 1 unique_values-"+column_number+"; sleep 4\" '")
+        os.system("gnome-terminal -e 'bash -c \"cd Documents/GitHub/MP-SPDZ/;./shamir-party.x -p 2 unique_values-"+column_number+"; sleep 4\" '")
+        unique_values = stream.read()
+        for j in range(0,int(unique_values)):
+            root.children[i-1].add_child(Node(str(2)+"-"+column_number+"-"+str(j+1)+"-"+shapeTuple[0],j+1,shapeTuple[0]))
+    #calculate the biggest entropy among the other remaining columns
+
+preorder(root)
 """
 if test in string:
     print('All items in the array are the same')
